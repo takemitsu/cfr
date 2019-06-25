60bd871684140413f0c5c0056467bb5b243adb9f
Takemitsu <takemitsu@notespace.jp>
2019-06-25 13:13:53 +0900

    edit project responsive

--- resources/js/components/ProjectEdit.vue
+++ resources/js/components/ProjectEdit.vue
@@ -1,63 +1,66 @@
 <template>
-    <section id="project-edit">
+    <section id="project-edit" class="row justify-content-center">
+        <div class="col-xl-6 col-lg-6 col-md-8 col-sm-12">
 
-        <h3 style="margin-top: 10px;" v-if="form.id">Project Edit</h3>
-        <h3 style="margin-top: 10px;" v-else>New Project</h3>
+            <h3 style="margin-top: 10px;" v-if="form.id">Project Edit</h3>
+            <h3 style="margin-top: 10px;" v-else>New Project</h3>
 
-        <div v-show="!is_confirm">
+            <div v-show="!is_confirm">
 
-            <div class="form-group">
-                <label>公式サイトURL:</label>
-                <div class="input-group">
-                    <input v-model="form.url" class="form-control" placeholder="公式サイトURL">
-                    <div class="input-group-append">
-                        <button type="button" class="btn btn-outline-primary" @click="getOgp()">OGP取得</button>
+                <div class="form-group">
+                    <label>公式サイトURL:</label>
+                    <div class="input-group">
+                        <input v-model="form.url" class="form-control" placeholder="公式サイトURL">
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-outline-primary" @click="getOgp()">OGP取得</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="form-group">
-                <label>プロジェクトタイトル:</label>
-                <input v-model="form.title" class="form-control" placeholder="プロジェクトタイトル">
-            </div>
+                <div class="form-group">
+                    <label>プロジェクトタイトル:</label>
+                    <input v-model="form.title" class="form-control" placeholder="プロジェクトタイトル">
+                </div>
 
-            <div class="form-group">
-                <label>説明等</label>
-                <textarea v-model="form.description" class="form-control" rows="6" placeholder="description"></textarea>
-            </div>
+                <div class="form-group">
+                    <label>説明等</label>
+                    <textarea v-model="form.description" class="form-control" rows="6"
+                              placeholder="description"></textarea>
+                </div>
 
-            <div class="form-group">
-                image url:
-                <input v-model="form.image_url" class="form-control" placeholder="image url">
-                <div v-if="form.image_url">
-                    <img :src="form.image_url">
+                <div class="form-group">
+                    image url:
+                    <input v-model="form.image_url" class="form-control" placeholder="image url">
+                    <div v-if="form.image_url">
+                        <img :src="form.image_url">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="button" class="btn btn-primary" @click="change_confirm()">check</button>
+                </div>
+
+            </div>
+            <div v-show="is_confirm">
+                <div class="form-group">
+                    <div v-if="form.image_url">
+                        <img :src="form.image_url">
+                        <hr>
+                    </div>
+                    <div>プロジェクトタイトル</div>
+                    <div style="font-weight: bold;margin-bottom: 20px;">{{form.title}}</div>
+                    <div>{{form.description}}</div>
+                    <div style="margin-top: 20px;">公式サイトURL</div>
+                    <div>{{form.url}}</div>
+                </div>
+
+                <div class="form-group">
+                    <button type="button" class="btn btn-secondary" @click="is_confirm=false">edit</button>
+                    <button type="button" class="btn btn-primary" @click="submit()">finish</button>
                 </div>
             </div>
 
-            <div class="form-group">
-                <button type="button" class="btn btn-primary" @click="change_confirm()">check</button>
-            </div>
-
         </div>
-        <div v-show="is_confirm">
-            <div class="form-group">
-                <div v-if="form.image_url">
-                    <img :src="form.image_url">
-                    <hr>
-                </div>
-                <div>プロジェクトタイトル</div>
-                <div style="font-weight: bold;margin-bottom: 20px;">{{form.title}}</div>
-                <div>{{form.description}}</div>
-                <div style="margin-top: 20px;">公式サイトURL</div>
-                <div>{{form.url}}</div>
-            </div>
-
-            <div class="form-group">
-                <button type="button" class="btn btn-secondary" @click="is_confirm=false">edit</button>
-                <button type="button" class="btn btn-primary" @click="submit()">finish</button>
-            </div>
-        </div>
-
 
     </section>
 </template>
